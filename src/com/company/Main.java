7df60d1ae06d1057f65c912b49c04a2b9b46bbb2
Stevens Qiu <stevensqiu@gmail.com>
2020-06-09 20:20:04 -0700

    get functions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,5 +14,20 @@ public class Main {
         String fileName = "test.js";
         List<String> content = Files.readAllLines(Paths.get(fileName));
         System.out.println(content);
+        List<String> functions = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (String s: content) {
+            if (sb.toString().isEmpty() && s.split("\\s")[0].equals("function")) {
+                sb.append(s);
+            } else if (s.equals("}")) {
+                sb.append(s);
+                functions.add(sb.toString());
+                sb.setLength(0);
+            } else if (sb.length() > 0) {
+                sb.append(s);
+            }
+        }
+
+        System.out.println(functions);
     }
 }
