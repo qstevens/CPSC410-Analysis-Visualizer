894bae9980656569f8fc860011a608dede8b20b5
Stevens Qiu <stevensqiu@gmail.com>
2020-06-09 20:09:44 -0700

    take input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        //
+    public static void main(String[] args) throws IOException {
+        // Read file
+        String fileName = "test.js";
+        List<String> content = Files.readAllLines(Paths.get(fileName));
+        System.out.println(content);
     }
 }
